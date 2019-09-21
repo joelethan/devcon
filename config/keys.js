60e81e7f898cf36902f5c236590b9d3017da2808
Katusiime Joel Ian <joelethan@users.noreply.github.com>
2019-09-22 00:11:00 +0300

    Connect to the mongoose DB

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+import dotenv from 'dotenv';
+dotenv.config();
+module.exports = {
+    mongoURI: process.env.DB_CONNECT
+}
--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
+
+// DB config
+const db =require('./config/keys').mongoURI
+
+// connect to DB
+mongoose
+    .connect(db,{ useUnifiedTopology: true },)
+    .then(()=> console.log('Connected'))
+    .catch(err => console.log(err))
 
 app.get('/', (req, res) => res.send('Hello world'),);
 
